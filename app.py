7247bafae0d4fabcc3c6f615aa0a3b7628d96073
SUMANTH <sumanthchukka9092@gmail.com>
2026-03-03 18:55:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ st.set_page_config(layout="wide")
 
 st.title("📊 Global Population Dashboard")
 
-power_bi_url = "PASTE_YOUR_EMBED_LINK_HERE"
+power_bi_url = "https://adityagroup-my.sharepoint.com/:u:/g/personal/24p35a4208_acet_ac_in/IQCZMbekSQWRT6et8HsWAVbsAcAtNVpDCjLCid0x3KEwwGc?e=MyoSxr"
+
 
 components.iframe(power_bi_url, height=750)
