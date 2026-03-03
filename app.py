f5d59d7dfa05fa2cae26a8da298b1e7a2d195282
SUMANTH <sumanthchukka9092@gmail.com>
2026-03-03 19:36:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
+# Google verification meta tag
+components.html(
+    """
+    <meta name="google-site-verification" content="-SFsJYFzgpk9IHnfjY5-D1gatuCSUZnvIwWv4Xhazbs" />
+    """,
+    height=0,
+)
 
 st.title("📊 Global Population Dashboard")
 
@@ -9,4 +16,5 @@ power_bi_url = "https://app.powerbi.com/view?r=eyJrIjoiZDNjYjc5N2EtNTJjZS00YmFiL
 
 
 components.iframe(power_bi_url, height=750)
+
 
